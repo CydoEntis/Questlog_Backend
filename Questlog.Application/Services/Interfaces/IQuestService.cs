451323f753e04750842b1ac2db79514ff3d8797c
CydoEntis <cydoentis@gmail.com>
2024-09-25 07:59:43 -0400

    Added IQuestService interface

--- Questlog.Application/Services/Interfaces/IQuestService.cs
+++ Questlog.Application/Services/Interfaces/IQuestService.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Questlog.Domain.Entities;
 
 namespace Questlog.Application.Services.Interfaces
 {
-    internal interface IQuestService
+    public interface IQuestService
     {
+        Task<IEnumerable<Quest>> GetAllQuestsForUser(string userId);
+        Task<Quest> GetQuest(int questId, string userId);
+        Task<int> CreateQuest(Quest quest, string userId);
+        Task<Quest> UpdateQuest(Quest quest, string userId);
+        Task DeleteQuest(int id, string userId);
     }
 }
