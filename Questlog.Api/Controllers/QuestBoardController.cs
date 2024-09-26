b3fd6d2d2f34f76793209e9e0e2835bea79ebaa8
CydoEntis <cydoentis@gmail.com>
2024-09-26 13:56:13 -0400

    Added Get QuestBoard by id method to QuestBoard Controller

--- Questlog.Api/Controllers/QuestBoardController.cs
+++ Questlog.Api/Controllers/QuestBoardController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Questlog.Api.Models;
 using Questlog.Application.Common.DTOs.MainQuest;
 using Questlog.Application.Common.DTOs.QuestBoard;
+using Questlog.Application.Services.Implementations;
 using Questlog.Application.Services.Interfaces;
 using Questlog.Domain.Entities;
 using System.Net;
@@ -50,7 +51,40 @@ namespace Questlog.Api.Controllers
             }
         }
 
+        [HttpGet("{id:int}", Name = "GetQuestBoard")]
+        public async Task<ActionResult<ApiResponse>> GetQuestBoard(int id)
+        {
+            if (id <= 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("InvalidId", new List<string> { "ID must be greater than zero." });
+                return BadRequest(_response);
+            }
 
+            string userId = HttpContext.Items["UserId"] as string;
 
+            try
+            {
+                var questBoard = await _questBoardService.GetQuestBoard(id, userId);
+                _response.Result = _mapper.Map<CreateQuestBoardRequestDTO>(questBoard);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("NotFound", new List<string> { ex.Message });
+                return NotFound(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("ServerError", new List<string> { "An error occurred while retrieving the Quest Board." });
+                return StatusCode((int)HttpStatusCode.InternalServerError, _response);
+            }
+        }
     }
 }
