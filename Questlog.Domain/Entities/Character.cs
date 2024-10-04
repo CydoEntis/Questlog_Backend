2147baad0861822885a4a0309f4a6f3a925042a2
CydoEntis <cydoentis@gmail.com>
2024-10-03 20:00:52 -0400

    Added Character Model and Unlockable Model and established relationship

---
+++ Questlog.Domain/Entities/Character.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questlog.Domain.Entities
+{
+    public class Character
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(12)]
+        [MinLength(3)]
+        public string DisplayName { get; set; }
+        [Required]
+        public Archetype Archetype { get; set; }
+        [Required]
+        public int CurrentExp { get; set; } = 0;
+        [Required]
+        public int ExpToNextLevel { get; set; } = 100;
+
+
+        public List<Unlockable> Inventory { get; set; } = new List<Unlockable>();
+    }
+}
---
+++ Questlog.Domain/Entities/Unlockable.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Questlog.Domain.Entities
+{
+    public class Unlockable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(2)]
+        [MaxLength(20)]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
+        public string Description { get; set; }
+        [Required]
+        [Range(5, 5000, ErrorMessage = "Cost must be at least 5, but no more than 5000")]
+        public int Cost { get; set; }
+        [Required]
+        public Archetype Archetype { get; set; }
+
+        [ForeignKey("Character")]
+        public int CharacterId { get; set; }
+
+        public Character Character { get; set; }
+    }
+}
