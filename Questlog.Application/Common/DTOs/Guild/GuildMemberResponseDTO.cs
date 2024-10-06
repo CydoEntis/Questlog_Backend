56e232959a9d2f10b4387c1786ab0c1c48475a80
CydoEntis <cydoentis@gmail.com>
2024-10-06 16:22:52 -0400

    Added GuildMember Create Request and response DTOs

---
+++ Questlog.Application/Common/DTOs/Guild/GuildMemberResponseDTO.cs
@@ -0,0 +1,23 @@
+﻿using Questlog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questlog.Application.Common.DTOs.Guild
+{
+    public record GuildMemberResponseDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public int GuildId { get; set; }
+        [Required]
+        public DateTime JoinedOn { get; set; }
+    }
+}
