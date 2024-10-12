6eb56cdbb8bca33a6f6785dca05d15be28b47f59
CydoEntis <cydoentis@gmail.com>
2024-10-11 20:25:08 -0400

    Updated error messages inside of UpdateGuildDetailsRequestDTO and CreateGuildRequestDTO

--- Questlog.Application/Common/DTOs/Guild/Requests/CreateGuildRequestDTO.cs
+++ Questlog.Application/Common/DTOs/Guild/Requests/CreateGuildRequestDTO.cs
@@ -12,17 +12,18 @@ namespace Questlog.Application.Common.DTOs.Guild.Requests
 {
     public record CreateGuildRequestDTO
     {
-        [Required]
-        [MinLength(3)]
-        [MaxLength(25)]
-        public string Name { get; set; }
+        [Required(ErrorMessage = "Guild name is required")]
+        [MinLength(3, ErrorMessage = "Guild name must be atleast 3 characters")]
+        [MaxLength(20, ErrorMessage = "Guild name cannot exceed 20 characters")]
+        public required string Name { get; set; }
 
-        [Required]
-        [MinLength(5)]
-        [MaxLength(100)]
-        public string Description { get; set; }
+        [Required(ErrorMessage = "Guild description is required")]
+        [MinLength(5, ErrorMessage = "Guild description must be atleast 5 characters")]
+        [MaxLength(50, ErrorMessage = "Guild description cannot exceed 50 characters")]
+        public required string Description { get; set; }
+   
+        [Required(ErrorMessage = "Guild color is required")]
+        public required string Color { get; set; }
 
-        [Required]
-        public string Color { get; set; }
     }
 }
--- Questlog.Application/Common/DTOs/Guild/Requests/UpdateGuildDetailsRequestDTO.cs
+++ Questlog.Application/Common/DTOs/Guild/Requests/UpdateGuildDetailsRequestDTO.cs
@@ -11,16 +11,20 @@ namespace Questlog.Application.Common.DTOs.Guild.Requests
     {
         [Required(ErrorMessage = "Guild id is required")]
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Guild name is required")]
         [MinLength(3, ErrorMessage = "Guild name must be atleast 3 characters")]
         [MaxLength(20, ErrorMessage = "Guild name cannot exceed 20 characters")]
         public required string Name { get; set; }
+
         [Required(ErrorMessage = "Guild description is required")]
         [MinLength(5, ErrorMessage = "Guild description must be atleast 5 characters")]
-        [MaxLength(100, ErrorMessage = "Guild description cannot exceed 100 characters")]
+        [MaxLength(50, ErrorMessage = "Guild description cannot exceed 50 characters")]
         public required string Description { get; set; }
+
         [Required(ErrorMessage = "Guild leader id is required")]
         public required string GuildLeaderId { get; set; }
+
         [Required(ErrorMessage = "Guild color is required")]
         public required string Color { get; set; }
     }
