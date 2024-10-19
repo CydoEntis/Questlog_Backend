e02d9e1753fbd2c344973e2fe5df5acf95b53c9a
CydoEntis <cydoentis@gmail.com>
2024-10-19 12:04:48 -0400

    Added DueDate to campaigned and updated DTOs

--- Questlog.Application/Common/DTOs/Campaign/Responses/CreateCampaignResponseDto.cs
+++ Questlog.Application/Common/DTOs/Campaign/Responses/CreateCampaignResponseDto.cs
@@ -7,6 +7,10 @@ namespace Questlog.Application.Common.DTOs.Guild.Responses
         public int Id { get; set; }
         public string Name { get; set; }
         public string Color { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
         public List<GetMemberAvatarResponseDto> Members { get; set; }
     }
 }
