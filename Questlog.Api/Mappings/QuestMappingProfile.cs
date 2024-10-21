3736c6247e77214e747190b1e511c9eb5f2af372
CydoEntis <cydoentis@gmail.com>
2024-10-21 17:40:51 -0400

    Added mapping profiles for both Quest and Subquest

---
+++ Questlog.Api/Mappings/QuestMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Questlog.Application.Common.DTOs.Quest;
+using Questlog.Application.Common.DTOs.Quest.Request;
+using Questlog.Domain.Entities;
+
+namespace Questlog.Api.Mappings;
+
+public class QuestMappingProfile : Profile
+{
+    public QuestMappingProfile()
+    {
+        CreateMap<Quest, CreateQuestResponseDto>().ReverseMap();
+        CreateMap<Quest, CreateQuestRequestDto>().ReverseMap();
+        CreateMap<Quest, GetQuestResponseDto>().ReverseMap();
+        CreateMap<Quest, UpdateQuestRequestDto>().ReverseMap();
+        CreateMap<Quest, UpdateQuestResponseDto>().ReverseMap();
+    }
+}
---
+++ Questlog.Api/Mappings/SubquestMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Questlog.Application.Common.DTOs.Subquest.Request;
+using Questlog.Application.Common.DTOs.Subquest.Response;
+using Questlog.Domain.Entities;
+
+namespace Questlog.Api.Mappings;
+
+public class SubquestMappingProfile : Profile
+{
+    public SubquestMappingProfile()
+    {
+        CreateMap<Subquest, CreateSubquestRequestDto>().ReverseMap();
+        CreateMap<Subquest, CreateSubquestResponseDto>().ReverseMap();
+    }
+}
