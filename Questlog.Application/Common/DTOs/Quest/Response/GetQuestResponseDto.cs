786bbabd181a5b1f4f0df39e546b3e79f52e7d45
CydoEntis <cydoentis@gmail.com>
2024-10-30 18:20:44 -0400

    Updated Quest response dto to send back priority instead of old difficulty

--- Questlog.Application/Common/DTOs/Quest/Response/GetQuestResponseDto.cs
+++ Questlog.Application/Common/DTOs/Quest/Response/GetQuestResponseDto.cs
@@ -9,7 +9,7 @@ public record GetQuestResponseDto()
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public string Difficulty { get; set; }
+    public string Priority { get; set; }
     public bool isCompleted { get; set; }
     public DateTime CreatedAt { get; set; } 
     public DateTime UpdatedAt { get; set; }
