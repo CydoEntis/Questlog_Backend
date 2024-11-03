91d328e738b26db9596a5b5c9f499c1a941c7412
CydoEntis <cydoentis@gmail.com>
2024-11-03 11:57:35 -0500

    Added constants related to level up exp and awarded exp values

---
+++ Questlog.Application/Common/Constants/LevelUpConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace Questlog.Application.Common.Constants;
+
+public class LevelUpConstants
+{
+    private const int CriticalXPReward = 200;
+    private const int HighXPReward = 150;
+    private const int MediumXPReward = 100;
+    private const int LowXPReward = 50;
+
+    private const int CriticalCurrencyReward = 100;
+    private const int HighCurrencyReward = 75;
+    private const int MediumCurrencyReward = 50;
+    private const int LowCurrencyReward = 25;
+}
